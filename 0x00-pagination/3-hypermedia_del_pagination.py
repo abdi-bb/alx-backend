57e0cc05bf0cc6a0f18364f9c2dbd7721b1fbcd3
Abdi B <abdiberhe@gmail.com>
2023-10-23 13:31:33 +0300

    Update 3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,29 +40,35 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        # Get the indexed dataset
-        indexed_dataset = self.indexed_dataset()
+    '''
+    A deletion-resilient method that returns a dict of hypermedia dict
+    '''
+    indexed_dataset = self.indexed_dataset()
+    dataset_length = len(indexed_dataset)
 
-        # Verify that the requested index is within a valid range
-        assert index is not None and 0 <= index < len(indexed_dataset)
+    # Verify that the requested index is within a valid range
+    if index is not None and (index < 0 or index >= dataset_length):
+        raise ValueError("Invalid index")
 
-        if index is None:
-            index = 0
-        next_index = None
-        page_data = []
-        page_data_count = 0
+    if index is None:
+        index = 0
 
-        for idx, item in indexed_dataset.items():
-            if idx >= index and page_data_count < page_size:
-                page_data.append(item)
-                page_data_count += 1
-                continue
+    next_index = None
+    page_data = []
+    page_data_count = 0
+
+    for idx in range(index, dataset_length):
+        item = indexed_dataset.get(idx)
+        if item is not None:
+            page_data.append(item)
+            page_data_count += 1
             if page_data_count == page_size:
-                next_index = idx
+                next_index = idx + 1
                 break
-        return {
-            'index': index,
-            'data': page_data,
-            'page_size': page_size,
-            'next_index': next_index,
-        }
+
+    return {
+        'index': index,
+        'data': page_data,
+        'page_size': page_size,
+        'next_index': next_index,
+    }
