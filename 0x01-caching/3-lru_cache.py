3612f298d0f6732cfd7799e7386694b67f52b065
Abdi Berhe <abdiberhe@gmail.com>
2023-10-25 23:22:37 +0300

    [Class]: LRUCache class that manages cache based on LRU order

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''
+Module: '3-lru_cache'
+Class: 'LRUCache' that inherits from BaseCaching
+'''
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''
+    Class LRUCache that manages cache based on LRU
+    '''
+
+    def __init__(self):
+        '''Overloading the parent constructor'''
+        super().__init__()
+        self.key_chronology = []
+
+    def put(self, key, item):
+        '''
+        Ties the item with the key in the parent's cache_data dict
+        '''
+        if key and item:
+            self.cache_data[key] = item
+            if key not in self.key_chronology:
+                self.key_chronology.append(key)
+            else:
+                self.key_chronology.append(
+                    self.key_chronology.pop(self.key_chronology.index(key)))
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                lru_key = self.key_chronology.pop(0)
+                del self.cache_data[lru_key]
+                print(f'DISCARD: {lru_key}')
+
+    def get(self, key):
+        '''
+        Returns the value tied to the key, if it exists
+        '''
+        if key in self.cache_data:
+            self.key_chronology.append(
+                self.key_chronology.pop(self.key_chronology.index(key)))
+        return self.cache_data.get(key)
